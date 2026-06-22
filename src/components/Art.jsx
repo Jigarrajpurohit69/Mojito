0bcfd7c9cb02fe48821a97db54f674e8ad0e1610
Jigarrajpurohit69 <jigarrajpurohit82@email.com>
2026-06-23 01:14:35 +0530

    made till glass moves according to arrows

--- src/components/Art.jsx
+++ src/components/Art.jsx
@@ -1,7 +1,47 @@
+import { useMediaQuery } from "react-responsive"
 import { featureLists, goodLists } from "../../constants"
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
  
 const Art = () => {
+
+    const isMobile = useMediaQuery({maxWidth: 767});
+
+    useGSAP(()=>{
+        const start = isMobile ? 'top 20%' : 'top top';
+
+
+        const maskTImeline = gsap.timeline({
+            scrollTrigger:{
+                trigger:'#art',
+                start,
+                end: 'bottom center',
+                scrub:1.5,
+                pin: true
+            }
+        })
+
+        maskTImeline
+        .to('.will-fade',{
+            opacity:0,
+            stagger:0.5,
+            ease: 'power1.inOut',
+        })
+        .to('.masked-img',{
+            scale:1.3,
+            maskPosition: 'center',
+            maskSize:'400%',
+            duration:3,
+            ease:'power1.inOut'
+        })
+        .to('.masked-content',{
+            opacity:1,
+            duration:2,
+            ease:'power1.inOut'
+        })
+    })
+
     return (
         <div id="art" >
             <div className="container mx-auto h-full pt-20">
@@ -31,6 +71,16 @@ const Art = () => {
                             </li>
                         ))}
                     </ul>
+                </div>
+
+                <div className="masked-container">
+                    <h2 className="will-fade">Sip-worthy Perfection</h2>
+                    <div className="masked-content">
+                        <h3>Made with Craft, Poured with Passion</h3>
+                        <p>This isn't just a drink. it's a carefully crafted moment made just
+                            for you 
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
